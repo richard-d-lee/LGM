4bdb72a007b605794ea661432329dae4697e2cb6
Richard Lee <richdelee@gmail.com>
2021-08-25 15:58:18 -0600

    set up routes file

---
+++ routes.js
@@ -0,0 +1,8 @@
+module.exports = function(app){
+
+    app.get('/login', function(req, res){
+        res.send('hello world')
+    });
+
+    //other routes..
+}
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
+require('./routes')(app);
 const PORT = 3000;
 const path = require("path");
 
 app.use(express.static('./client/dist')); // Host your dist folder up to the server
 app.use(express.json()); // Alternative to BodyParser
-
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname, './client/dist/index.html'), function(err) {
       if (err) {
