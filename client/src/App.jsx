9f5bcea0ea5a23aed11da436438eba9678e5fa76
Richard Lee <richdelee@gmail.com>
2021-09-24 09:20:10 -0600

    corrected login error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,7 +18,7 @@ class App extends React.Component {
         this.state = {
             username: '',
             page: "home",
-            logged: true,
+            logged: false,
         }
         this.setLogged = this.setLogged.bind(this)
         this.openNav = this.openNav.bind(this);
